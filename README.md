826d4cce88f3a1b865b13535e8cedf8f3b214043
Black Cloud <koolight89@gmail.com>
2022-12-06 19:49:08 +0900

    front 70%

---
+++ README.md
@@ -0,0 +1,2 @@
+# LanguageWizard
+<img width="959" alt="potpol" src="https://user-images.githubusercontent.com/93169318/205241633-0cf8ec29-2ee3-4797-b8fe-b4882bc3a7c0.png">
--- app.py
+++ app.py
@@ -16,19 +16,63 @@ def run():
     text = request.args.get('input')
      
     if select == '0':
-        return render_template('magician.html', input1=text, output1='Successful translation', selected0='selected')
+        return render_template(
+            'ai.html', 
+            input1=text, 
+            output1='Successful translation', 
+            selected0='selected',
+            model='Transformer',
+            data='ai_tranformer_data',
+            library1='Pytorch : 1.9.0',
+            library2='transformers : 4.30.0',
+            link="https://www.google.com/",
+            )
+
     elif select == '1':
-        return render_template('magician.html', input1=text, output1='요약 성공', selected1='selected')
+        return render_template(
+            'ai.html',
+            input1=text, 
+            output1='요약 성공', 
+            selected1='selected',
+            model='T5',
+            data='ai_sum_data',
+            library1='Pytorch : 1.9.0',
+            library2='transformers : 4.30.0',
+            library3='test',
+            link="https://www.naver.com/",
+            )
+    
     elif select == '2':
-        return render_template('magician.html', input1=text, output1='생성 성공', selected2='selected')
-    elif select == '3':
-        return render_template('magician.html', input1=text, output1='분류 성공', selected3='selected')
+        return render_template(
+            'ai.html',
+            input1=text, 
+            output1='챗봇 성공', 
+            selected2='selected',
+            model='GPT-2',
+            data='ai_chatbot_data',
+            library1='Pytorch : 1.9.0',
+            library2='transformers : 4.30.0',
+            library3='test',
+            library4='test2',
+            link="https://www.google.com/",
+            )
+
+    else:
+        return render_template(
+            'ai.html', 
+            input1=text, 
+            output1='분류 성공', 
+            selected3='selected',
+            model='BERT',
+            data='ai_classification_data',
+            library1='Pytorch : 1.9.0',
+            library2='transformers : 4.30.0',
+            link="https://www.google.com/",
+            )
 
 
 @app.route('/o', methods=['GET'])
 def o():
-
-
     inp_text = request.args.get('input')
     out_text = request.args.get('output')
     print('===o :', inp_text)
@@ -36,6 +80,7 @@ def o():
 
     return render_template('magician.html')
     
+
 @app.route('/x', methods=['GET'])
 def x():
 
@@ -44,7 +89,8 @@ def x():
     print('===x :', inp_text)
     print('===x :', out_text)
 
-    return render_template('magician_x.html', input1=inp_text)
+    return render_template('x.html', input1=inp_text)
+
 
 @app.route('/insert', methods=['GET'])
 def insert():
@@ -53,9 +99,10 @@ def insert():
     print('===insert :', inp_text)
     print('===insert :', out_text)
     if out_text == '':
-        return render_template('magician_x.html', input1=inp_text)
+        return render_template('x.html', input1=inp_text)
 
     return render_template('magician.html', input1=inp_text)
+
 
 if __name__ == '__main__':
     app.run(
